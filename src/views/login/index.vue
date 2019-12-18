3c8db9210847c1a131252f73677a25db4f368e33
hhn <1761293315@qq.com>
2019-12-18 17:04:15 +0800

    初始化项目-页面分析-新建登录和主页/登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <div class="login">login</div>
+  <div class="login">
+      <el-card class="login-card">
+
+      </el-card>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style>
+<style lang='less'>
+.login {
+    height: 100vh;
+    background-image: url("../../assets/img/login_dg.jpg");
+    background-size: cover;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .login-card{
+        width: 400px;
+        height: 300px;
+    }
+}
 
 </style>
