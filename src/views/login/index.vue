5326b04dd2d35c874f83dbe82246347f13dab3fe
hhn <1761293315@qq.com>
2019-12-18 19:16:22 +0800

    数据绑定及校验(上)

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,56 +1,78 @@
 <template>
   <div class="login">
-      <el-card class="login-card">
-          <div class="login-img">
-              <img src="../../assets/img/logo_index.png" alt="">
-          </div>
-          <el-form style="margin-top:20px">
-              <el-form-item>
-                  <el-input placeholder="请输入手机号"></el-input>
-              </el-form-item>
-              <el-form-item>
-                  <el-input placeholder="请输入验证码" class="input-two"></el-input>
-                  <el-button plain class="login-button">发送验证码</el-button>
-              </el-form-item>
-              <el-form-item>
-                  <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
-              </el-form-item>
-              <el-form-item>
-                  <el-button style="width:100%"  type="primary">登录</el-button>
-              </el-form-item>
-          </el-form>
-      </el-card>
+    <el-card class="login-card">
+      <div class="login-img">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <el-form style="margin-top:20px" :model="loginForm" :rules='loginVerify'>
+        <el-form-item  prop='phone'>
+          <el-input v-model="loginForm.phone" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop='auth'>
+          <el-input v-model="loginForm.auth" placeholder="请输入验证码" class="input-two"></el-input>
+          <el-button plain class="login-button">发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="loginForm.agree">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="width:100%" type="primary">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        phone: '',
+        auth: '',
+        agree: false
+      },
+      loginVerify: {
+        phone: [{ required: true, message: '手机号不能为空' },
+          { pattern: /^1[3456789]\d{9}$/, message: '请输入合法的手机号' }],
+        auth: [{ required: true, message: '验证码不能为空' },
+          { pattern: /^\d{6}$/, message: '验证码格式错误' }],
+        agree: [{ validator: function (rule, value, callback) {
+          if (value) {
+            callback()
+          } else {
+            callback(new Error('你必须无条件同意'))
+          }
+        } }]
+      }
+    }
+  }
+}
 </script>
 
 <style lang='less'>
 .login {
-    height: 100vh;
-    background-image: url("../../assets/img/login_dg.jpg");
-    background-size: cover;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    .login-card{
-        width: 440px;
-        height: 320px;
-        .login-img{
-            text-align: center;
-            img {
-                height: 35px;
-            }
-        }
-        .input-two{
-            width: 60%;
-        }
-        .login-button{
-            float: right;
-        }
+  height: 100vh;
+  background-image: url("../../assets/img/login_dg.jpg");
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    width: 440px;
+    height: 320px;
+    .login-img {
+      text-align: center;
+      img {
+        height: 35px;
+      }
     }
+    .input-two {
+      width: 60%;
+    }
+    .login-button {
+      float: right;
+    }
+  }
 }
-
 </style>
