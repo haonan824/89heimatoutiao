77bb65c3e190ced0f4e28bc339b432a1c6d2704d
hhn <1761293315@qq.com>
2019-12-21 16:29:54 +0800

    黑马头条PC-主页模块-导航守卫

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui' // 引入UI
+import './permission/index.js'
 import axios from 'axios'
 import Component from './components/index'
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,7 @@ const routes = [
     component: Home,
     children: [
       { path: '', component: home2 }
+      // { path: '/home/comment', component: () => import('../views/comment/bread-crumb.vue') }
     ]
   }
   // {
